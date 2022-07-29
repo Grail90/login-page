a057661c583ff34969ffe4640dfae7763e8c0df3
Grail90 <97138168+Grail90@users.noreply.github.com>
2022-07-29 06:58:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var img = ["images/1.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpg"];
+/*var img = ["images/1.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpg"];
 
 
 var background = document.getElementById('container')
